bfb5d1623337f0214652dd27b32d4fb211034840
Alisa Deplova <504448@niuitmo.ru>
2025-10-07 21:21:18 +0300

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -5,14 +5,16 @@
 1) encrypt: выполняет символьное шифрование строки со сдвигом символов.
 2)decrypt: расшифровывает строку, зашифрованную с помощью "encrypt".
 """
+ASCII_START: int =32
+ASCII_END: int = 126
+ALPHABET: int = ASCII_END - ASCII_START + 1
 
-
-def encrypt(plaintext, shift):
+def encrypt(plaintext: str, shift: int) -> str:
     """
     Шифрует  все печатные символы ASCII (с кодами от 32 до 126 включительно).
 
     Каждый сдвиг делается на определенное количество позиций.
-    Если символ переходит границы допустимого диапазона( от 32 до 126 ), то сдвиг "заворачивается" в пределах указанного диапазона символов.
+    Сдвиг производится в пределах диапазона ASCII_START-ASCII_END
     Символы вне этого диапазона не изменяются
     Args:
         plaintext (str): Входная строка для шифрования
@@ -25,13 +27,11 @@ def encrypt(plaintext, shift):
 
     for symbol in plaintext:
         code = ord(symbol)
-        if 32 <= code<= 126:
-            shifted_code = code + shift
-            while shifted_code >126:
-                shifted_code = shifted_code - 95 #Вычитаем 95(126-32+1 = 95) чтобы завернуть в начало
-            while shifted_code < 32:
-                shifted_code = shifted_code + 95 #Прибавляем 95 чтобы завернуть в конец
-            result += chr(shifted_code)
+        if ASCII_START <= code <= ASCII_END:
+            num = code - ASCII_START
+            index = (num + shift) % ALPHABET
+            new_code = ASCII_START + index
+            result += chr(new_code)
         else:
             result += symbol
     return result
@@ -39,7 +39,7 @@ def encrypt(plaintext, shift):
 
 
 
-def decrypt(ciphertext, shift):
+def decrypt(ciphertext: str, shift: int) -> str:
     """
     Расшифровывает все зашифрованные печатные символы ASCII (с кодами от 32 до 126 включительно).
 
@@ -59,23 +59,16 @@ def decrypt(ciphertext, shift):
     for symbol in ciphertext:
         code = ord(symbol)
 
-        if 32 <= code <= 126:
-            shifted_code = code - shift
-            while shifted_code < 32:
-                shifted_code = shifted_code + 95  # Прибавляем 95 чтобы завернуть в конец
-            while shifted_code >126:
-                shifted_code = shifted_code - 95 #Вычитаем 95(126-32+1 = 95) чтобы завернуть в начало
-            result += chr(shifted_code)
-        else:
-            result += symbol
+        for symbol in ciphertext:
+            code = ord(symbol)
+            if ASCII_START <= code <= ASCII_END:
+                num = code - ASCII_START
+                index = (num - shift) % ALPHABET
+                new_code = ASCII_START + index
+                result += chr(new_code)
+            else:
+                result += symbol
+        return result
 
-    return result
 
-
-text='priv'
-shift= 45
-
-a= encrypt(text,shift)
-b= decrypt(a,shift)
-print(a)
-print(b)
+print(encrypt("H
